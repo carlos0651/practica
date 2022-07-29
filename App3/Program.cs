5d745230c815d4826123b08effd17bc006e7d8f8
CARLOS MAURICIO GOMEZ COREAS <carlosgomezmayo2003@gmail.com>
2022-07-29 13:17:28 -0600

    Tercer programa

---
+++ App3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace App3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 52.24F;
+            double num2 = 52.24;
+            decimal num3 = 52.54M;
+            Console.WriteLine("{0} {1} {2} ", num1, num2, num3);
+        }
+    }
+}
