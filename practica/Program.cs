c9b0eb060edf0aa81173342c505a86d7969f8323
CARLOS MAURICIO GOMEZ COREAS <carlosgomezmayo2003@gmail.com>
2022-07-29 17:07:49 -0600

    primer programa

--- practica/Program.cs
+++ practica/Program.cs
@@ -6,16 +6,10 @@ namespace practica
     {
         static void Main(string[] args)
         {
-            //declaracion de variables 
             int num1 = 5;
-            int num2 = 1;
-            var total = num1 + num2;
-
             string name = "Carlos";
             int edad = 19;
-            //impresion de variables 
-            Console.WriteLine("la suma es: " + total);
-            Console.WriteLine(name + " " + edad);
+            Console.WriteLine("su nombre es " + name + " y su edad es: " + edad);
         }
     }
 }
