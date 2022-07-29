da74f001331dee1fc7444d8b8b984a6d6f374577
CARLOS MAURICIO GOMEZ COREAS <carlosgomezmayo2003@gmail.com>
2022-07-29 17:28:34 -0600

    quinto programa

--- App5/Program.cs
+++ App5/Program.cs
@@ -6,16 +6,18 @@ namespace App5
     {
         static void Main(string[] args)
         {
-            int num1, num2;
-            Console.WriteLine("introduzca un numero");
-            num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("introduzca otro numero");
-            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese un numero");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingres otro numero");
+            int num2 = int.Parse(Console.ReadLine());
+            var suma = num1 + num2;
+            var resta = num1 - num2;
+            var multiplicacion = num1 * num2;
+            var division = num1 / num2;
 
-            Console.WriteLine("La suma entre los numeros es: " + (num1 + num2));
-            Console.WriteLine("La resta entre los numeros es: " + (num1 - num2));
-            Console.WriteLine("La multiplicacion entre los numeros es: " + (num1 * num2));
-            Console.WriteLine("La division entre los numeros es: " + (num1 / num2));
-        }
+            Console.WriteLine("La suma de los numeros es: {0}", suma);
+            Console.WriteLine("La suma de los numeros es: {0}", resta);
+            Console.WriteLine("La suma de los numeros es: {0}", multiplicacion);
+            Console.WriteLine("La suma de los numeros es: {0}", division);
     }
 }
