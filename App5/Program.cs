35030c1f6ef7e89cc1c0b21310391ae634b0a286
CARLOS MAURICIO GOMEZ COREAS <carlosgomezmayo2003@gmail.com>
2022-07-29 13:47:06 -0600

    quinto programa

---
+++ App5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace App5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("introduzca un numero");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduzca otro numero");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("La suma entre los numeros es: " + (num1 + num2));
+            Console.WriteLine("La resta entre los numeros es: " + (num1 - num2));
+            Console.WriteLine("La multiplicacion entre los numeros es: " + (num1 * num2));
+            Console.WriteLine("La division entre los numeros es: " + (num1 / num2));
+        }
+    }
+}
