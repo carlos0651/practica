04256160d6cbd1e7dcbf219f590efaa663824038
CARLOS MAURICIO GOMEZ COREAS <carlosgomezmayo2003@gmail.com>
2022-07-29 17:17:45 -0600

    cuarto programa

--- App4/Program.cs
+++ App4/Program.cs
@@ -6,14 +6,11 @@ namespace App4
     {
         static void Main(string[] args)
         {
-            //pedir y almacenar datos de texto
             Console.WriteLine("Introduzca su nombre ");
             string name = Console.ReadLine();
-            //pedir, convertir y almacenar datos numericos 
-            Console.WriteLine("Introduzca su edad ");
+            Console.WriteLine("Ingrese su edad ");
             int edad = int.Parse(Console.ReadLine());
-            //imprimir todos los datos 
-            Console.WriteLine("Su nombre es: {0} y tiene {1} años de edad", name, edad);
+            Console.WriteLine("Su nombre es: {1} y tiene {0} años de edad", edad, name);
         }
     }
 }
